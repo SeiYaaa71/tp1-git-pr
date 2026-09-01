2d07edab956ea8417e54928271525e55c71a08fa
matteo <matteo.bastide06@gmail.com>
2026-09-01 17:22:50 +0200

    feat: fonction Add de base

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func Add(a, b int) int { return a + b }
+func Multiply(a, b int) int { return a * b }
+
 
 func main() {
 fmt.Println("Add(2, 3) =", Add(2, 3))
+fmt.Println("Multiply(4, 5) =", Multiply(4, 5))
 }

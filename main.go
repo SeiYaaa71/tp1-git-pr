f9254f941fdae1c72fd6288efd499461e42bd980
matteo <matteo.bastide06@gmail.com>
2026-09-02 14:06:39 +0200

    feat: ajout de la fonction Divide

--- main.go
+++ main.go
@@ -10,7 +10,15 @@ func Multiply(a, b int) int {
 	return a * b
 }
 
+func Divide(a, b int) int {
+	return a / b
+}
+
 func main() {
 	fmt.Println("Add(2, 3) =", Add(2, 3))
 	fmt.Println("Multiply(4, 5) =", Multiply(4, 5))
+
+	total := 10
+	parts := len([]string{}) // vaut 0
+	fmt.Println("Divide =", Divide(total, parts))
 }

871ceecdbd9702ca6e6387b667e0b59d9466b81e
matteo <matteo.bastide06@gmail.com>
2026-09-02 13:58:04 +0200

    feat: ajout de la fonction Multiply

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
-func Add(a, b int) int { return a + b }
+func Add(a, b int) int {
+	return a + b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
 
 func main() {
-fmt.Println("Add(2, 3) =", Add(2, 3))
+	fmt.Println("Add(2, 3) =", Add(2, 3))
+	fmt.Println("Multiply(4, 5) =", Multiply(4, 5))
 }
